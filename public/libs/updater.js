55fae97e03b24f9a7ee35ae09539526559a63cef
Quang Lam <quang.lam2807@gmail.com>
2020-05-15 16:57:07 +0700

    Fix update checker is not pointing to new repo

--- public/libs/updater.js
+++ public/libs/updater.js
@@ -9,18 +9,14 @@ const customizedFetch = require('./customized-fetch');
 
 const checkForUpdates = (silent) => {
   console.log('Checking for updates...'); // eslint-disable-line no-console
-  customizedFetch('https://api.github.com/repos/atomery/webcatalog/releases/latest')
+  customizedFetch('https://api.github.com/repos/atomery/juli/releases/latest')
     .then((res) => res.json())
-    .then((release) => {
-      const v = release.tag_name;
-      return customizedFetch(`https://raw.githubusercontent.com/atomery/webcatalog/${v}/package.json`);
-    })
-    .then((res) => res.json())
-    .then((fetchedJson) => {
-      if (semver.gt(fetchedJson.templateVersion, packageJson.version)) {
+    .then((release) => release.tag_name.substring(1))
+    .then((latestVersion) => {
+      if (semver.gt(latestVersion, packageJson.version)) {
         dialog.showMessageBox(mainWindow.get(), {
           type: 'info',
-          message: `An update (${appJson.name} ${fetchedJson.templateVersion}) is available. Use the latest version of WebCatalog to update this app.`,
+          message: `An update (${appJson.name} ${latestVersion}) is available. Use the latest version of WebCatalog to update this app.`,
           buttons: ['OK'],
           cancelId: 0,
           defaultId: 0,
