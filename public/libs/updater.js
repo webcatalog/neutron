500794acad4c24318570b7bdc06882f44fab3ea8
Quang Lam <quang.lam2807@gmail.com>
2020-05-19 09:31:35 +0700

    Fix updater fails as GitHub API requests exceed server limit (#11)

--- public/libs/updater.js
+++ public/libs/updater.js
@@ -7,10 +7,39 @@ const appJson = require('../app.json');
 
 const customizedFetch = require('./customized-fetch');
 
+// GitHub API has rate limit & node-fetch doesn't support caching out-of-the-box
+// Use caching and conditional request to ensure it's under limit
+// See https://developer.github.com/v3/#conditional-requests
+// Most responses return an ETag header.
+// Many responses also return a Last-Modified header.
+// You can use the values of these headers to make subsequent
+// requests to those resources using the If-None-Match
+// and If-Modified-Since headers, respectively.
+// If the resource has not changed, the server will return a 304 Not Modified.
+let cachedResponse = null;
 const checkForUpdates = (silent) => {
   console.log('Checking for updates...'); // eslint-disable-line no-console
-  customizedFetch('https://api.github.com/repos/atomery/juli/releases/latest')
-    .then((res) => res.json())
+  const opts = {};
+  if (cachedResponse) {
+    opts.headers = {
+      'If-None-Match': cachedResponse.etag,
+    };
+  }
+  customizedFetch('https://api.github.com/repos/atomery/juli/releases/latest', opts)
+    .then((res) => {
+      if (res.status === 304) {
+        return cachedResponse.json;
+      }
+
+      return res.json()
+        .then((json) => {
+          cachedResponse = {
+            etag: res.headers.get('etag'),
+            json,
+          };
+          return cachedResponse.json;
+        });
+    })
     .then((release) => release.tag_name.substring(1))
     .then((latestVersion) => {
       if (semver.gt(latestVersion, packageJson.version)) {
