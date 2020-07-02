12ed6f76547d484cf9f7ade4eb474af56012cd94
Quang Lam <quang.lam2807@gmail.com>
2020-07-02 23:43:51 +0700

    Use juli.webcatalogapp.com API to check for updates

--- public/libs/updater.js
+++ public/libs/updater.js
@@ -10,29 +10,18 @@ const customizedFetch = require('./customized-fetch');
 
 const checkForUpdates = (silent) => {
   console.log('Checking for updates...'); // eslint-disable-line no-console
-  // avoid using GitHub API as it has rate limit (60 requests per hour)
-  customizedFetch('https://github.com/atomery/juli/releases.atom')
-    .then((res) => res.text())
-    .then((xmlData) => {
-      const releases = xmlParser.parse(xmlData).feed.entry;
-
-      // find stable version
-      for (let i = 0; i < releases.length; i += 1) {
-        const release = releases[i];
-        const tagName = release.id.split('/').pop();
-        const version = tagName.substring(1);
-        if (!semver.prerelease(version)) {
-          return version;
-        }
-      }
-
-      return Promise.reject(new Error('Server returns no valid updates.'));
-    })
+  // use in-house API
+  // to avoid using GitHub API as it has rate limit (60 requests per hour)
+  // to avoid bugs with instead of https://github.com/atomery/juli/releases.atom
+  // https://github.com/atomery/webcatalog/issues/890
+  customizedFetch('https://juli.webcatalogapp.com/releases/latest.json')
+    .then((res) => res.json())
+    .then((data) => data.version)
     .then((latestVersion) => {
       if (semver.gt(latestVersion, packageJson.version)) {
         dialog.showMessageBox(mainWindow.get(), {
           type: 'info',
-          message: `An update (${appJson.name} ${latestVersion}) is available. Use the latest version of WebCatalog to update this app.`,
+          message: `An update (${appJson.name} ${latestVersion}) is available. Open WebCatalog to update this app.`,
           buttons: ['OK'],
           cancelId: 0,
           defaultId: 0,
