bfc0ee8a56a3c7599cbb2da448bbf94ae9f7dee6
Quang Lam <quang.lam2807@gmail.com>
2020-08-13 16:16:12 +0700

    Fix Sentry release string varies between generated apps (#64)

--- public/libs/sentry.js
+++ public/libs/sentry.js
@@ -1,18 +1,9 @@
 const electron = require('electron');
 const { init } = require('@sentry/electron');
-const isDev = require('electron-is-dev');
 
-const getSentryPreference = () => {
-  const isRenderer = (process && process.type === 'renderer');
-  if (isRenderer) {
-    return electron.ipcRenderer.sendSync('get-preference', 'sentry');
-  }
-  // eslint-disable-next-line global-require
-  return require('./preferences').getPreference('sentry');
-};
+const isRenderer = (process && process.type === 'renderer');
 
-if (!isDev && getSentryPreference()) {
-  init({
-    dsn: 'https://c243f91cf27e4fc789c66840318809ec@o433326.ingest.sentry.io/5388233',
-  });
-}
+init({
+  dsn: 'https://c243f91cf27e4fc789c66840318809ec@o433326.ingest.sentry.io/5388233',
+  release: isRenderer ? electron.remote.app.getVersion() : electron.app.getVersion(),
+});
