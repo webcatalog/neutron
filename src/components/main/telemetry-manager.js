9bd4f90be15ed82083887f50025dde89a51a931a
Quang Lam <quang.lam2807@gmail.com>
2020-11-03 14:27:51 +0700

    Add "focus app" telemetry event

--- src/components/main/telemetry-manager.js
+++ src/components/main/telemetry-manager.js
@@ -8,11 +8,21 @@ const TelemetryManager = () => {
   // run after setUserProperties
   // https://blog.logrocket.com/post-hooks-guide-react-call-order
   useEffect(() => {
-    if (window.mode === 'main' || window.mode === 'menubar') {
-      const appJson = window.remote.getGlobal('appJson');
-      amplitude.getInstance().setUserProperties({ registered: Boolean(appJson.registered) });
-      amplitude.getInstance().logEvent('webcatalog-engine: start app');
-    }
+    const appJson = window.remote.getGlobal('appJson');
+    amplitude.getInstance().setUserProperties({ registered: Boolean(appJson.registered) });
+    amplitude.getInstance().logEvent('webcatalog-engine: start app');
+
+    // this is important to track usage correctly
+    // if not, we will miss usage data when users keep the app open and switch back later
+    // instead of quitting and restarting the app
+    const win = window.remote.getCurrentWindow();
+    const logFocus = () => {
+      amplitude.getInstance().logEvent('focus app');
+    };
+    win.on('focus', logFocus);
+    return () => {
+      win.removeListener('focus', logFocus);
+    };
   }, []);
   return null;
 };
