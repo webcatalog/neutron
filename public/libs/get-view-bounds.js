99d011907ae48999ec213fe56e6afaf3c2bf593e
Quang Lam <quang.lam2807@gmail.com>
2020-11-10 21:30:21 +0700

    Update get-view-bounds.js

--- public/libs/get-view-bounds.js
+++ public/libs/get-view-bounds.js
@@ -8,7 +8,6 @@ const getViewBounds = (contentSize, findInPage = false, height, width) => {
     && global.attachToMenubar
     && !global.sidebar) || global.navigationBar;
 
-  const sidebarWidth = process.platform === 'darwin' ? 68 : 48;
   const sidebarWidth = 68;
   const titlebarHeight = process.platform === 'darwin' ? 22 : 32;
   const offsetTitlebar = showTitleBar ? titlebarHeight : 0;
