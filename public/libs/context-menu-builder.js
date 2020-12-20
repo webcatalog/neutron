3e76b9ce38093c68de0fdedadff58023ab872830
Quang Lam <quang.lam2807@gmail.com>
2020-12-20 16:02:14 +0700

    Fix "document is not defined" bug (#238)

--- public/libs/context-menu-builder.js
+++ public/libs/context-menu-builder.js
@@ -3,13 +3,11 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-param-reassign */
-/* global Image */
 // Ported from
 // https://github.com/electron-userland/electron-spellchecker/blob/master/src/context-menu-builder.js
 
 const {
   clipboard,
-  nativeImage,
   shell,
   Menu,
   MenuItem,
@@ -337,8 +335,7 @@ module.exports = class ContextMenuBuilder {
 
     const copyImage = new MenuItem({
       label: this.stringTable.copyImage(),
-      click: () => this.convertImageToBase64(menuInfo.srcURL,
-        (dataURL) => clipboard.writeImage(nativeImage.createFromDataURL(dataURL))),
+      click: () => target.copyImageAt(menuInfo.x, menuInfo.y),
     });
 
     menu.append(copyImage);
@@ -420,31 +417,5 @@ module.exports = class ContextMenuBuilder {
 
     menu.append(inspect);
     return menu;
-  }
-
-  /**
-   * Converts an image to a base-64 encoded string.
-   *
-   * @param  {String} url           The image URL
-   * @param  {Function} callback    A callback that will be invoked with the result
-   * @param  {String} outputFormat  The image format to use, defaults to 'image/png'
-   */
-  convertImageToBase64(url, callback, outputFormat = 'image/png') {
-    let canvas = document.createElement('CANVAS');
-    const ctx = canvas.getContext('2d');
-    const img = new Image();
-    img.crossOrigin = 'Anonymous';
-
-    img.onload = () => {
-      canvas.height = img.height;
-      canvas.width = img.width;
-      ctx.drawImage(img, 0, 0);
-
-      const dataURL = canvas.toDataURL(outputFormat);
-      canvas = null;
-      callback(dataURL);
-    };
-
-    img.src = url;
   }
 };
