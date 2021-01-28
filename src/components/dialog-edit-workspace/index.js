9bcb69537101f3c4d7d9b936b151b12c5895c8d7
Quang Lam <quang.lam2807@gmail.com>
2021-01-29 00:34:41 +0700

    x

--- src/components/dialog-edit-workspace/index.js
+++ src/components/dialog-edit-workspace/index.js
@@ -288,8 +288,8 @@ const EditWorkspace = ({
               if (!homeUrlError && isMailApp) {
                 return 'Email app detected.';
               }
-              if (!homeUrl && window.remote.getGlobal('appJson').url) {
-                return `Defaults to ${window.remote.getGlobal('appJson').url}.`;
+              if (!homeUrl && appJson.url) {
+                return `Defaults to ${appJson.url}.`;
               }
               return homeUrlError;
             })()}
