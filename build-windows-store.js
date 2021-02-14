4fa424391ac58289a732b269587501af9a6053a8
Quang Lam <quang.lam2807@gmail.com>
2021-02-14 10:07:37 +0700

    Create build-windows-store.js

---
+++ build-windows-store.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable header/header */
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+const builder = require('electron-builder');
+/*
+CLOSED SOURCE
+*/
