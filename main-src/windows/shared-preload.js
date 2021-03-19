523df60eaf3aaae6938e40c8e79163949740b3e1
Quang Lam <quang.lam2807@gmail.com>
2021-03-20 04:06:42 +0700

    Upgrade dependencies (#371)

--- main-src/windows/shared-preload.js
+++ main-src/windows/shared-preload.js
@@ -6,10 +6,10 @@ const {
   remote,
 } = require('electron');
 const contextMenu = require('electron-context-menu');
-const isDev = require('electron-is-dev');
 const machineId = require('node-machine-id');
 
 // Activate the Sentry Electron SDK as early as possible in every process.
+const isDev = process.env.NODE_ENV !== 'production';
 if (!isDev && ipcRenderer.sendSync('get-preference', 'sentry')) {
   // eslint-disable-next-line global-require
   require('../libs/sentry');
