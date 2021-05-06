b24a9a2b8619753eb49338e3de26c0cb8bcdbec3
Quang Lam <quang.lam2807@gmail.com>
2021-05-06 16:27:00 +0700

    Add electron-notarize dependency

--- build-standalone.js
+++ build-standalone.js
@@ -7,6 +7,8 @@ const path = require('path');
 const fs = require('fs-extra');
 const glob = require('glob');
 const del = require('del');
+const { notarize } = require('electron-notarize');
+const { exec } = require('child_process');
 
 /*
 CLOSED SOURCE
