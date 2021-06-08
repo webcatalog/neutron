c01ec2cc95dcba1c6dae7899a2e39c9a79a16782
Quang Lam <quang.lam2807@gmail.com>
2021-06-08 07:07:50 +0700

    Refactor account/service preferences UI code

---
+++ src/components/workspace-preferences/section-advanced/index.js
@@ -0,0 +1,263 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+import connectComponent from '../../../helpers/connect-component';
+import roundTime from '../../../helpers/round-time';
+import getStaticGlobal from '../../../helpers/get-static-global';
+import getUtmSource from '../../../helpers/get-utm-source';
+
+import {
+  requestOpenInBrowser,
+  requestRequestReloadWorkspaceDialog,
+} from '../../../senders';
+
+import { updateForm } from '../../../state/dialog-workspace-preferences/actions';
+
+import autoRefreshIntervals from '../../../constants/auto-refresh-intervals';
+
+import DialogInternalUrls from '../../shared/dialog-internal-urls';
+import DialogRefreshInterval from '../../shared/dialog-refresh-interval';
+
+const styles = (theme) => ({
+  link: {
+    cursor: 'pointer',
+    fontWeight: 500,
+    outline: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+    '&:focus': {
+      textDecoration: 'underline',
+    },
+  },
+  selectRoot: {
+    borderRadius: theme.spacing(0.5),
+    fontSize: '0.84375rem',
+  },
+  selectRootExtraMargin: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  select: {
+    paddingTop: theme.spacing(1),
+    paddingRight: 26,
+    paddingBottom: theme.spacing(1),
+    paddingLeft: theme.spacing(1.5),
+  },
+  refreshEvery: {
+    float: 'right',
+    paddingRight: theme.spacing(1),
+  },
+});
+
+const SectionAdvanced = ({
+  autoRefresh,
+  autoRefreshInterval,
+  autoRefreshOnlyWhenInactive,
+  classes,
+  internalUrlRule,
+  onOpenDialogInternalUrls,
+  onOpenDialogRefreshInterval,
+  onUpdateForm,
+  formAutoRefresh,
+  formAutoRefreshInterval,
+  formAutoRefreshOnlyWhenInactive,
+  formInternalUrlRule,
+}) => (
+  <>
+    <List disablePadding dense>
+      <ListItem button onClick={onOpenDialogInternalUrls}>
+        <ListItemText
+          primary="Internal URLs"
+          secondary={(() => {
+            if (formInternalUrlRule != null) {
+              return `/^${formInternalUrlRule}$/i`;
+            }
+            return `Same as global (${internalUrlRule ? `/^${internalUrlRule}$/i` : 'Not set'})`;
+          })()}
+        />
+        <ChevronRightIcon color="action" />
+      </ListItem>
+      <Divider />
+      <ListItem>
+        <ListItemText
+          primary="Reload web pages automatically"
+        />
+        <Select
+          value={formAutoRefresh != null ? formAutoRefresh : 'global'}
+          onChange={(e) => {
+            onUpdateForm({
+              autoRefresh: e.target.value !== 'global' ? e.target.value : null,
+            });
+            const workspaceId = getStaticGlobal('workspacePreferencesWorkspaceId');
+            requestRequestReloadWorkspaceDialog(workspaceId);
+          }}
+          variant="filled"
+          disableUnderline
+          margin="dense"
+          classes={{
+            root: classes.select,
+          }}
+          className={classnames(classes.selectRoot, classes.selectRootExtraMargin)}
+        >
+          <MenuItem dense value="global">{`Same as global (${autoRefresh ? 'Yes' : 'No'})`}</MenuItem>
+          <MenuItem dense value>Yes</MenuItem>
+          <MenuItem dense value={false}>No</MenuItem>
+        </Select>
+      </ListItem>
+      {formAutoRefresh && (
+        <>
+          <ListItem>
+            <ListItemText primary="Reload every" classes={{ primary: classes.refreshEvery }} />
+            <Select
+              value={formAutoRefreshInterval || autoRefreshInterval}
+              onChange={(e) => {
+                if (e.target.value === 'custom') {
+                  onOpenDialogRefreshInterval();
+                  return;
+                }
+                onUpdateForm({
+                  autoRefreshInterval: e.target.value,
+                });
+                const workspaceId = getStaticGlobal('workspacePreferencesWorkspaceId');
+                requestRequestReloadWorkspaceDialog(workspaceId);
+              }}
+              variant="filled"
+              disableUnderline
+              margin="dense"
+              classes={{
+                root: classes.select,
+              }}
+              className={classnames(classes.selectRoot, classes.selectRootExtraMargin)}
+            >
+              {autoRefreshIntervals.map((opt) => (
+                <MenuItem key={opt.value} dense value={opt.value}>{opt.name}</MenuItem>
+              ))}
+              {(() => {
+                const val = formAutoRefreshInterval || autoRefreshInterval;
+                const isCustom = autoRefreshIntervals
+                  .find((interval) => interval.value === val) == null;
+                if (isCustom) {
+                  const time = roundTime(val);
+                  return (
+                    <MenuItem dense value={val}>
+                      {`${time.value} ${time.unit}`}
+                    </MenuItem>
+                  );
+                }
+                return null;
+              })()}
+              <MenuItem dense value="custom">Custom</MenuItem>
+            </Select>
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Only reload on inactivity"
+              secondary={(
+                <>
+                  <span>Keep certain apps from logging </span>
+                  <span>out automatically when you are away. </span>
+                  <span
+                    role="link"
+                    tabIndex={0}
+                    className={classes.link}
+                    onClick={() => requestOpenInBrowser(`https://help.webcatalog.app/article/25-how-to-prevent-apps-from-logging-me-out-on-inactivity?utm_source=${getUtmSource()}`)}
+                    onKeyDown={(e) => {
+                      if (e.key !== 'Enter') return;
+                      requestOpenInBrowser(`https://help.webcatalog.app/article/25-how-to-prevent-apps-from-logging-me-out-on-inactivity?utm_source=${getUtmSource()}`);
+                    }}
+                  >
+                    Learn more
+                  </span>
+                  <span>.</span>
+                </>
+              )}
+            />
+            <Select
+              value={formAutoRefreshOnlyWhenInactive != null ? formAutoRefreshOnlyWhenInactive : 'global'}
+              onChange={(e) => {
+                onUpdateForm({
+                  autoRefreshOnlyWhenInactive: e.target.value !== 'global' ? e.target.value : null,
+                });
+                const workspaceId = getStaticGlobal('workspacePreferencesWorkspaceId');
+                requestRequestReloadWorkspaceDialog(workspaceId);
+              }}
+              variant="filled"
+              disableUnderline
+              margin="dense"
+              classes={{
+                root: classes.select,
+              }}
+              className={classnames(classes.selectRoot, classes.selectRootExtraMargin)}
+            >
+              <MenuItem dense value="global">{`Same as global (${autoRefreshOnlyWhenInactive ? 'Yes' : 'No'})`}</MenuItem>
+              <MenuItem dense value>Yes</MenuItem>
+              <MenuItem dense value={false}>No</MenuItem>
+            </Select>
+          </ListItem>
+        </>
+      )}
+    </List>
+    <DialogInternalUrls />
+    <DialogRefreshInterval />
+  </>
+);
+
+SectionAdvanced.defaultProps = {
+  internalUrlRule: null,
+  formAutoRefresh: null,
+  formAutoRefreshInterval: null,
+  formAutoRefreshOnlyWhenInactive: null,
+  formInternalUrlRule: null,
+};
+
+SectionAdvanced.propTypes = {
+  autoRefresh: PropTypes.bool.isRequired,
+  autoRefreshInterval: PropTypes.number.isRequired,
+  autoRefreshOnlyWhenInactive: PropTypes.bool.isRequired,
+  classes: PropTypes.object.isRequired,
+  internalUrlRule: PropTypes.string,
+  onOpenDialogInternalUrls: PropTypes.func.isRequired,
+  onOpenDialogRefreshInterval: PropTypes.func.isRequired,
+  onUpdateForm: PropTypes.func.isRequired,
+
+  formAutoRefresh: PropTypes.bool,
+  formAutoRefreshInterval: PropTypes.number,
+  formAutoRefreshOnlyWhenInactive: PropTypes.bool,
+  formInternalUrlRule: PropTypes.string,
+};
+
+const mapStateToProps = (state) => ({
+  autoRefresh: state.preferences.autoRefresh,
+  autoRefreshInterval: state.preferences.autoRefreshInterval,
+  autoRefreshOnlyWhenInactive: state.preferences.autoRefreshOnlyWhenInactive,
+  internalUrlRule: state.preferences.internalUrlRule,
+  formAskForDownloadPath: state.dialogWorkspacePreferences.form.askForDownloadPath,
+  formAutoRefresh: state.dialogWorkspacePreferences.form.autoRefresh,
+  formAutoRefreshInterval: state.dialogWorkspacePreferences.form.autoRefreshInterval,
+  formInternalUrlRule: state.dialogWorkspacePreferences.form.internalUrlRule,
+});
+
+const actionCreators = {
+  updateForm,
+};
+
+export default connectComponent(
+  SectionAdvanced,
+  mapStateToProps,
+  actionCreators,
+  styles,
+);
---
+++ src/components/workspace-preferences/section-developers/index.js
@@ -0,0 +1,131 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+import connectComponent from '../../../helpers/connect-component';
+
+import { open as openDialogCodeInjection } from '../../../state/dialog-code-injection/actions';
+import { open as openDialogCustomUserAgent } from '../../../state/dialog-custom-user-agent/actions';
+import { updateForm } from '../../../state/dialog-workspace-preferences/actions';
+
+import DialogCodeInjection from '../../shared/dialog-code-injection';
+import DialogCustomUserAgent from '../../shared/dialog-custom-user-agent';
+
+const SectionDevelopers = ({
+  cssCodeInjection,
+  customUserAgent,
+  jsCodeInjection,
+  onOpenDialogCodeInjection,
+  onOpenDialogCustomUserAgent,
+  formCssCodeInjection,
+  formCustomUserAgent,
+  formJsCodeInjection,
+}) => (
+  <>
+    <List disablePadding dense>
+      <ListItem button onClick={onOpenDialogCustomUserAgent}>
+        <ListItemText
+          primary="Custom User Agent"
+          secondary={(() => {
+            if (formCustomUserAgent != null) {
+              return formCustomUserAgent;
+            }
+            return `Same as global (${customUserAgent || 'Not set'})`;
+          })()}
+          secondaryTypographyProps={{ noWrap: true }}
+        />
+        <ChevronRightIcon color="action" />
+      </ListItem>
+      <Divider />
+      <ListItem
+        button
+        onClick={() => {
+          onOpenDialogCodeInjection('js');
+        }}
+      >
+        <ListItemText
+          primary="JS Code Injection"
+          secondary={(() => {
+            if (formJsCodeInjection != null) {
+              return formJsCodeInjection;
+            }
+            return `Same as global (${jsCodeInjection || 'Not set'})`;
+          })()}
+          secondaryTypographyProps={{ noWrap: true }}
+        />
+        <ChevronRightIcon color="action" />
+      </ListItem>
+      <Divider />
+      <ListItem
+        button
+        onClick={() => {
+          onOpenDialogCodeInjection('css');
+        }}
+      >
+        <ListItemText
+          primary="CSS Code Injection"
+          secondary={(() => {
+            if (formCssCodeInjection != null) {
+              return formCssCodeInjection;
+            }
+            return `Same as global (${cssCodeInjection || 'Not set'})`;
+          })()}
+          secondaryTypographyProps={{ noWrap: true }}
+        />
+        <ChevronRightIcon color="action" />
+      </ListItem>
+    </List>
+    <DialogCodeInjection />
+    <DialogCustomUserAgent />
+  </>
+);
+
+SectionDevelopers.defaultProps = {
+  cssCodeInjection: null,
+  customUserAgent: null,
+  jsCodeInjection: null,
+  formCssCodeInjection: null,
+  formCustomUserAgent: null,
+  formJsCodeInjection: null,
+};
+
+SectionDevelopers.propTypes = {
+  cssCodeInjection: PropTypes.string,
+  customUserAgent: PropTypes.string,
+  jsCodeInjection: PropTypes.string,
+  onOpenDialogCodeInjection: PropTypes.func.isRequired,
+  onOpenDialogCustomUserAgent: PropTypes.func.isRequired,
+  formCustomUserAgent: PropTypes.string,
+  formJsCodeInjection: PropTypes.string,
+  formCssCodeInjection: PropTypes.string,
+};
+
+const mapStateToProps = (state) => ({
+  cssCodeInjection: state.preferences.cssCodeInjection,
+  customUserAgent: state.preferences.customUserAgent,
+  jsCodeInjection: state.preferences.jsCodeInjection,
+  formCssCodeInjection: state.dialogWorkspacePreferences.form.cssCodeInjection,
+  formCustomUserAgent: state.dialogWorkspacePreferences.form.customUserAgent,
+  formJsCodeInjection: state.dialogWorkspacePreferences.form.jsCodeInjection,
+});
+
+const actionCreators = {
+  openDialogCodeInjection,
+  openDialogCustomUserAgent,
+  updateForm,
+};
+
+export default connectComponent(
+  SectionDevelopers,
+  mapStateToProps,
+  actionCreators,
+);
