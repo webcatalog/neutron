f50d05049abf14f7f54f28f3acd1a18c6c6a655c
Quang Lam <quang.lam2807@gmail.com>
2021-07-28 03:09:34 +0700

    Fix Google prevents signing in because of security concerns

--- main-src/libs/views.js
+++ main-src/libs/views.js
@@ -359,7 +359,7 @@ const addViewAsync = async (browserWindow, workspace) => {
   // fix Google prevents signing in because of security concerns
   // https://github.com/webcatalog/webcatalog-app/issues/455
   // https://github.com/meetfranz/franz/issues/1720#issuecomment-566460763
-  const fakedEdgeUaStr = `${app.userAgentFallback} Edge/18.18875`;
+  const fakedFirefoxUaStr = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:90.0) Gecko/20100101 Firefox/90.0';
   const fakedSafariUaStr = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15';
   const adjustUserAgentByUrl = (contents, url, occasion) => {
     const currentUaStr = contents.userAgent;
@@ -368,17 +368,17 @@ const addViewAsync = async (browserWindow, workspace) => {
       if (currentUaStr !== customUserAgent) {
         contents.userAgent = customUserAgent;
         // eslint-disable-next-line no-console
-        console.log('Changed user agent to', fakedEdgeUaStr, 'based of user preference', 'when', occasion);
+        console.log('Changed user agent to', customUserAgent, 'based of user preference', 'when', occasion);
         return true;
       }
     }
 
     const navigatedDomain = extractDomain(url);
     if (navigatedDomain === 'accounts.google.com') {
-      if (currentUaStr !== fakedEdgeUaStr) {
-        contents.userAgent = fakedEdgeUaStr;
+      if (currentUaStr !== fakedFirefoxUaStr) {
+        contents.userAgent = fakedFirefoxUaStr;
         // eslint-disable-next-line no-console
-        console.log('Changed user agent to', fakedEdgeUaStr, 'for web compatibility URL: ', url, 'when', occasion);
+        console.log('Changed user agent to', fakedFirefoxUaStr, 'for web compatibility URL: ', url, 'when', occasion);
         return true;
       }
     } else if (navigatedDomain === 'meet.google.com') {
