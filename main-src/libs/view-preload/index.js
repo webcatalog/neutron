b389abce09a8f528c8838185dd064610751e7357
Quang Lam <quang.lam2807@gmail.com>
2021-11-22 19:00:13 -0600

    Fix NavigatorUAData.getHighEntropyValues() is not implemented

--- main-src/libs/view-preload/index.js
+++ main-src/libs/view-preload/index.js
@@ -11,6 +11,7 @@ const {
   disable: disableDarkMode,
   setFetchMethod: setFetchMethodDarkMode,
 } = require('darkreader');
+const os = require('os');
 
 const isMas = require('../is-mas');
 const getRecipe = require('../get-recipe');
@@ -412,21 +413,27 @@ webFrame.executeJavaScript(`
 /* Gmail - required for loading standard version (otherwise redirects to basic HTML) */
 // modified from https://github.com/minbrowser/min/blob/58927524e3cc16cc4f59bca09a6c352cec1a16ac/js/preload/siteUnbreak.js (Apache)
 const chromiumVersion = process.versions.chrome.split('.')[0];
+
 webFrame.executeJavaScript(`
-(function() {
+(() => {
+  const brands = [
+    { brand: "Chromium", version: "${chromiumVersion}" },
+    { brand: "Not A;Brand", version: "99" },
+  ];
   const simulatedUAData = {
-    brands: [
-      {brand: "Chromium", version: "${chromiumVersion}"},
-      {brand: "Not A;Brand", version: "99"}
-    ],
+    brands,
     mobile: false,
-    getHighEntropyValues: function() {
-      console.warn('getHighEntropyValues is unimplemented', arguments)
-      return null
-    }
-  }
-  Object.defineProperty(navigator, 'userAgentData', {get: () => simulatedUAData})
-})()
+    getHighEntropyValues: () => Promise.resolve({
+      architecture: "${process.platform}",
+      brands,
+      mobile: false,
+      model: "",
+      platformVersion: "${os.release()}",
+      uaFullVersion: "${process.versions.chrome}"
+    })
+  };
+  Object.defineProperty(navigator, 'userAgentData', {get: () => simulatedUAData });
+})();
 `);
 
 // enable pinch zooming (default behavior of Chromium)
