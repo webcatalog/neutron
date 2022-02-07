225baf15acc4fcad4b38fcd6823adf4659d287e3
Quang Lam <quang.lam2807@gmail.com>
2022-02-06 19:55:03 -0600

    [CU-2acdqz0] Fix screen sharing selection window shows blank page (#783)

--- src/invokers/index.js
+++ src/invokers/index.js
@@ -14,6 +14,7 @@ export const isDefaultCalendarAppAsync = () => window.ipcRenderer.invoke('is-def
 export const getIapFormattedPriceAsync = (productIdentifier) => window.ipcRenderer.invoke('get-iap-formatted-price', productIdentifier);
 export const getExtensionFromProfileAsync = (browserId, profileDirName) => window.ipcRenderer.invoke('get-extensions-from-profile', browserId, profileDirName);
 export const getExtensionSourcesAsync = () => window.ipcRenderer.invoke('get-extension-sources');
+export const getDesktopCapturerSourcesAsync = (options) => window.ipcRenderer.invoke('get-desktop-capturer-sources', options);
 
 // Permission
 export const getPermissionAuthTypeAsync = (authType) => window.ipcRenderer.invoke('get-permission-auth-status', authType);
