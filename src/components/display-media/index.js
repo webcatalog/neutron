e2f8f8d59169b4bc2eb6ef14d9d37c0650031683
dotrongkhang2000 <76832829+dotrongkhang2000@users.noreply.github.com>
2022-03-16 16:01:43 +0700

    [NEU-146] Replace connectComponent with hooks (3)

--- src/components/display-media/index.js
+++ src/components/display-media/index.js
@@ -1,8 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -10,12 +9,11 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
-
-import connectComponent from '../../helpers/connect-component';
+import { makeStyles } from '@material-ui/core';
 
 import { getDesktopCapturerSourcesAsync } from '../../invokers';
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     height: '100%',
     width: '100%',
@@ -29,76 +27,58 @@ const styles = (theme) => ({
     paddingLeft: theme.spacing(2),
     paddingRight: theme.spacing(2),
   },
-});
+}));
 
-class DisplayMedia extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sources: [],
-    };
-  }
+const DisplayMedia = () => {
+  const classes = useStyles();
+  const [sources, setSource] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     getDesktopCapturerSourcesAsync({ types: ['window', 'screen'] })
-      .then((sources) => {
-        this.setState({ sources });
+      .then((res) => {
+        setSource({ res });
       });
-  }
+  }, []);
 
-  render() {
-    const { sources } = this.state;
-    const { classes } = this.props;
+  const screenSources = sources.filter((source) => source.id.startsWith('screen'));
+  const windowSources = sources.filter((source) => source.id.startsWith('window'));
+  // remove first item as it is the display media window itself
+  windowSources.shift();
 
-    const screenSources = sources.filter((source) => source.id.startsWith('screen'));
-    const windowSources = sources.filter((source) => source.id.startsWith('window'));
-    // remove first item as it is the display media window itself
-    windowSources.shift();
-
-    return (
-      <div className={classes.root}>
-        <Typography variant="body2" className={classes.text}>
-          The app wants to use the contents of your screen. Choose what you’d like to share.
-        </Typography>
-        <List>
-          <ListSubheader disableSticky>Screens</ListSubheader>
-          {screenSources.map((source) => (
-            <ListItem
-              key={source.id}
-              button
-              onClick={() => {
-                window.ipcRenderer.send('display-media-selected', source.id);
-              }}
-            >
-              <ListItemText primary={source.name} />
-            </ListItem>
-          ))}
-          <Divider />
-          <ListSubheader disableSticky>Windows</ListSubheader>
-          {windowSources.map((source) => (
-            <ListItem
-              key={source.id}
-              button
-              onClick={() => {
-                window.ipcRenderer.send('display-media-selected', source.id);
-              }}
-            >
-              <ListItemText primary={source.name} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
-
-DisplayMedia.propTypes = {
-  classes: PropTypes.object.isRequired,
+  return (
+    <div className={classes.root}>
+      <Typography variant="body2" className={classes.text}>
+        The app wants to use the contents of your screen. Choose what you’d like to share.
+      </Typography>
+      <List>
+        <ListSubheader disableSticky>Screens</ListSubheader>
+        {screenSources.map((source) => (
+          <ListItem
+            key={source.id}
+            button
+            onClick={() => {
+              window.ipcRenderer.send('display-media-selected', source.id);
+            }}
+          >
+            <ListItemText primary={source.name} />
+          </ListItem>
+        ))}
+        <Divider />
+        <ListSubheader disableSticky>Windows</ListSubheader>
+        {windowSources.map((source) => (
+          <ListItem
+            key={source.id}
+            button
+            onClick={() => {
+              window.ipcRenderer.send('display-media-selected', source.id);
+            }}
+          >
+            <ListItemText primary={source.name} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 };
 
-export default connectComponent(
-  DisplayMedia,
-  null,
-  null,
-  styles,
-);
+export default DisplayMedia;
