6290ec5ad25d4a793cd1589815f4b50e455d472f
dotrongkhang2000 <76832829+dotrongkhang2000@users.noreply.github.com>
2022-03-17 17:38:44 +0700

    file 57

--- src/components/preferences/section-theme/index.js
+++ src/components/preferences/section-theme/index.js
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import React from 'react';
-import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import Divider from '@material-ui/core/Divider';
@@ -12,10 +11,12 @@ import ListItemText from '@material-ui/core/ListItemText';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core';
+
+import { useSelector } from 'react-redux';
 
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 
-import connectComponent from '../../../helpers/connect-component';
 import getWorkspaceFriendlyName from '../../../helpers/get-workspace-friendly-name';
 import camelCaseToSentenceCase from '../../../helpers/camel-case-to-sentence-case';
 
@@ -25,7 +26,7 @@ import {
   requestSetPreference,
 } from '../../../senders';
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   selectRoot: {
     borderRadius: theme.spacing(0.5),
     fontSize: '0.84375rem',
@@ -44,81 +45,64 @@ const styles = (theme) => ({
     width: theme.spacing(3),
     height: theme.spacing(3),
   },
-});
+}));
 
-const SectionTheme = ({
-  classes,
-  themeSource,
-  themeColor,
-}) => (
-  <List disablePadding dense>
-    <ListItem>
-      <ListItemText primary="Theme" />
-      <Select
-        value={themeSource}
-        onChange={(e) => requestSetPreference('themeSource', e.target.value)}
-        variant="filled"
-        disableUnderline
-        margin="dense"
-        classes={{
-          root: classes.select,
-        }}
-        className={classnames(classes.selectRoot, classes.selectRootExtraMargin)}
-      >
-        <MenuItem dense value="system">System default</MenuItem>
-        <MenuItem dense value="light">Light</MenuItem>
-        <MenuItem dense value="dark">Dark</MenuItem>
-      </Select>
-    </ListItem>
-    <Divider />
-    <ListItem>
-      <ListItemText primary="Color" />
-      <Select
-        value={themeColor == null ? 'none' : themeColor}
-        onChange={(e) => requestSetPreference('themeColor', e.target.value === 'none' ? null : e.target.value)}
-        variant="filled"
-        disableUnderline
-        margin="dense"
-        classes={{
-          root: classes.select,
-        }}
-        className={classnames(classes.selectRoot, classes.selectRootExtraMargin)}
-      >
-        <MenuItem dense value="none">None</MenuItem>
-        <MenuItem dense value="auto">
-          {`Matches with active ${getWorkspaceFriendlyName().toLowerCase()}'s color`}
-        </MenuItem>
-        {Object.keys(themeColors).map((val) => (
-          <MenuItem dense value={val} key={val}>
-            <ListItemIcon>
-              <Avatar className={classes.avatar} style={{ backgroundColor: themeColors[val][600] }}>{' '}</Avatar>
-            </ListItemIcon>
-            <ListItemText primary={camelCaseToSentenceCase(val)} />
+const SectionTheme = () => {
+  const classes = useStyles();
+
+  const themeSource = useSelector((state) => state.preferences.themeSource);
+  const themeColor = useSelector((state) => state.preferences.themeColor);
+
+  return (
+    <List disablePadding dense>
+      <ListItem>
+        <ListItemText primary="Theme" />
+        <Select
+          value={themeSource}
+          onChange={(e) => requestSetPreference('themeSource', e.target.value)}
+          variant="filled"
+          disableUnderline
+          margin="dense"
+          classes={{
+            root: classes.select,
+          }}
+          className={classnames(classes.selectRoot, classes.selectRootExtraMargin)}
+        >
+          <MenuItem dense value="system">System default</MenuItem>
+          <MenuItem dense value="light">Light</MenuItem>
+          <MenuItem dense value="dark">Dark</MenuItem>
+        </Select>
+      </ListItem>
+      <Divider />
+      <ListItem>
+        <ListItemText primary="Color" />
+        <Select
+          value={themeColor == null ? 'none' : themeColor}
+          onChange={(e) => requestSetPreference('themeColor', e.target.value === 'none' ? null : e.target.value)}
+          variant="filled"
+          disableUnderline
+          margin="dense"
+          classes={{
+            root: classes.select,
+          }}
+          className={classnames(classes.selectRoot, classes.selectRootExtraMargin)}
+        >
+          <MenuItem dense value="none">None</MenuItem>
+          <MenuItem dense value="auto">
+            {`Matches with active ${getWorkspaceFriendlyName().toLowerCase()}'s color`}
           </MenuItem>
-        ))}
-      </Select>
-    </ListItem>
-  </List>
-);
-
-SectionTheme.defaultProps = {
-  themeColor: null,
+          {Object.keys(themeColors).map((val) => (
+            <MenuItem dense value={val} key={val}>
+              <ListItemIcon>
+                <Avatar className={classes.avatar} style={{ backgroundColor: themeColors[val][600] }}>{' '}</Avatar>
+              </ListItemIcon>
+              <ListItemText primary={camelCaseToSentenceCase(val)} />
+            </MenuItem>
+          ))}
+        </Select>
+      </ListItem>
+    </List>
+  );
 };
 
-SectionTheme.propTypes = {
-  classes: PropTypes.object.isRequired,
-  themeSource: PropTypes.string.isRequired,
-  themeColor: PropTypes.string,
-};
-
-const mapStateToProps = (state) => ({
-  themeSource: state.preferences.themeSource,
-  themeColor: state.preferences.themeColor,
-});
-
-export default connectComponent(
-  SectionTheme,
-  mapStateToProps,
-  null,
-  styles,
-);
+export default SectionTheme;
