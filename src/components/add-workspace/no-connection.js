d63c07aa8d820a81b668e2e427cd193147cd9620
dotrongkhang2000 <76832829+dotrongkhang2000@users.noreply.github.com>
2022-03-17 13:17:51 +0700

    File 13

--- src/components/add-workspace/no-connection.js
+++ src/components/add-workspace/no-connection.js
@@ -7,9 +7,9 @@ import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import ErrorIcon from '@material-ui/icons/Error';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-const styles = {
+const useStyles = makeStyles(() => ({
   root: {
     alignItems: 'center',
     display: 'flex',
@@ -25,13 +25,14 @@ const styles = {
   tryAgainButton: {
     marginTop: 16,
   },
-};
+}));
 
 const NoConnection = (props) => {
   const {
-    classes,
     onTryAgainButtonClick,
   } = props;
+
+  const classes = useStyles();
 
   return (
     <div className={classes.root}>
@@ -61,8 +62,7 @@ const NoConnection = (props) => {
 };
 
 NoConnection.propTypes = {
-  classes: PropTypes.object.isRequired,
   onTryAgainButtonClick: PropTypes.func.isRequired,
 };
 
-export default withStyles(styles, { name: 'NoConnection' })(NoConnection);
+export default NoConnection;
