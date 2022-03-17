10f15d6971a12167ff5b032653f7866d0d8246a4
dotrongkhang2000 <76832829+dotrongkhang2000@users.noreply.github.com>
2022-03-17 17:38:36 +0700

    file 56

--- src/components/preferences/section-telemetry/index.js
+++ src/components/preferences/section-telemetry/index.js
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
@@ -11,7 +10,8 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Switch from '@material-ui/core/Switch';
 
-import connectComponent from '../../../helpers/connect-component';
+import { useSelector } from 'react-redux';
+
 import isWebcatalog from '../../../helpers/is-webcatalog';
 
 import {
@@ -19,65 +19,54 @@ import {
   requestSetPreference,
 } from '../../../senders';
 
-const SectionTelemetry = ({
-  sentry,
-  telemetry,
-}) => (
-  <List disablePadding dense>
-    <ListItem>
-      <ListItemText
-        primary="Allow the app to send anonymous crash reports"
-        secondary={isWebcatalog()
-          ? 'This preference is managed by WebCatalog app.'
-          : 'Help us quickly diagnose and fix bugs in the app.'}
-      />
-      <ListItemSecondaryAction>
-        <Switch
-          edge="end"
-          color="primary"
-          checked={sentry}
-          disabled={isWebcatalog()}
-          onChange={(e) => {
-            requestSetPreference('sentry', e.target.checked);
-            enqueueRequestRestartSnackbar();
-          }}
-        />
-      </ListItemSecondaryAction>
-    </ListItem>
-    <Divider />
-    <ListItem>
-      <ListItemText
-        primary="Allow the app to send anonymous usage data"
-        secondary={isWebcatalog()
-          ? 'This preference is managed by WebCatalog app.'
-          : 'Help us understand how to improve the product.'}
-      />
-      <ListItemSecondaryAction>
-        <Switch
-          edge="end"
-          color="primary"
-          checked={telemetry}
-          disabled={isWebcatalog()}
-          onChange={(e) => {
-            requestSetPreference('telemetry', e.target.checked);
-          }}
-        />
-      </ListItemSecondaryAction>
-    </ListItem>
-  </List>
-);
+const SectionTelemetry = () => {
+  const sentry = useSelector((state) => state.preferences.sentry);
+  const telemetry = useSelector((state) => state.preferences.telemetry);
 
-SectionTelemetry.propTypes = {
-  sentry: PropTypes.bool.isRequired,
-  telemetry: PropTypes.bool.isRequired,
+  return (
+    <List disablePadding dense>
+      <ListItem>
+        <ListItemText
+          primary="Allow the app to send anonymous crash reports"
+          secondary={isWebcatalog()
+            ? 'This preference is managed by WebCatalog app.'
+            : 'Help us quickly diagnose and fix bugs in the app.'}
+        />
+        <ListItemSecondaryAction>
+          <Switch
+            edge="end"
+            color="primary"
+            checked={sentry}
+            disabled={isWebcatalog()}
+            onChange={(e) => {
+              requestSetPreference('sentry', e.target.checked);
+              enqueueRequestRestartSnackbar();
+            }}
+          />
+        </ListItemSecondaryAction>
+      </ListItem>
+      <Divider />
+      <ListItem>
+        <ListItemText
+          primary="Allow the app to send anonymous usage data"
+          secondary={isWebcatalog()
+            ? 'This preference is managed by WebCatalog app.'
+            : 'Help us understand how to improve the product.'}
+        />
+        <ListItemSecondaryAction>
+          <Switch
+            edge="end"
+            color="primary"
+            checked={telemetry}
+            disabled={isWebcatalog()}
+            onChange={(e) => {
+              requestSetPreference('telemetry', e.target.checked);
+            }}
+          />
+        </ListItemSecondaryAction>
+      </ListItem>
+    </List>
+  );
 };
 
-const mapStateToProps = (state) => ({
-  sentry: state.preferences.sentry,
-  telemetry: state.preferences.telemetry,
-});
-
-export default connectComponent(
-  SectionTelemetry,
-  mapStateToProps,
-);
+export default SectionTelemetry;
