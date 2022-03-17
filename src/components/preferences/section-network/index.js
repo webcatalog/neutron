0c23232c68b5e4b787051492968dcfc87f01cd11
dotrongkhang2000 <76832829+dotrongkhang2000@users.noreply.github.com>
2022-03-17 17:38:00 +0700

    file 49

--- src/components/preferences/section-network/index.js
+++ src/components/preferences/section-network/index.js
@@ -2,11 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+
+import { useDispatch, useSelector } from 'react-redux';
 
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
@@ -23,56 +24,43 @@ const styles = (theme) => ({
     border: theme.palette.type === 'dark' ? 'none' : '1px solid rgba(0, 0, 0, 0.12)',
   },
 });
+const Preferences = () => {
+  const dispatch = useDispatch();
 
-const Preferences = ({
-  proxyMode,
-  onOpenDialogProxy,
-}) => (
-  <>
-    <List disablePadding dense>
-      <ListItem button onClick={onOpenDialogProxy}>
-        <ListItemText
-          primary="Proxy"
-          secondary={(() => {
-            switch (proxyMode) {
-              case 'fixed_servers': {
-                return 'Using proxy server.';
-              }
-              case 'pac_script': {
-                return 'Using PAC script (automatic proxy configuration script).';
-              }
-              case 'system': {
-                return 'Using system proxy configurations.';
-              }
-              default: {
-                return 'Not configured.';
-              }
-            }
-          })()}
-        />
-        <ChevronRightIcon color="action" />
-      </ListItem>
-    </List>
-    <DialogProxy />
-  </>
-);
+  const proxyMode = useSelector((state) => state.preferences.proxyMode);
 
-Preferences.propTypes = {
-  proxyMode: PropTypes.oneOf(['direct', 'fixed_servers', 'pac_script', 'system']).isRequired,
-  onOpenDialogProxy: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  proxyMode: state.preferences.proxyMode,
-});
-
-const actionCreators = {
-  openDialogProxy,
+  return (
+    <>
+      <List disablePadding dense>
+        <ListItem button onClick={dispatch(openDialogProxy)}>
+          <ListItemText
+            primary="Proxy"
+            secondary={(() => {
+              switch (proxyMode) {
+                case 'fixed_servers': {
+                  return 'Using proxy server.';
+                }
+                case 'pac_script': {
+                  return 'Using PAC script (automatic proxy configuration script).';
+                }
+                case 'system': {
+                  return 'Using system proxy configurations.';
+                }
+                default: {
+                  return 'Not configured.';
+                }
+              }
+            })()}
+          />
+          <ChevronRightIcon color="action" />
+        </ListItem>
+      </List>
+      <DialogProxy />
+    </>
+  );
 };
 
 export default connectComponent(
   Preferences,
-  mapStateToProps,
-  actionCreators,
   styles,
 );
