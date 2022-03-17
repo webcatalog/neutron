fa5aaa9d133d4260b320e19be439f914f3a943a8
dotrongkhang2000 <76832829+dotrongkhang2000@users.noreply.github.com>
2022-03-17 17:37:37 +0700

    file 44

--- src/components/preferences/section-language/index.js
+++ src/components/preferences/section-language/index.js
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
@@ -11,9 +10,9 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Switch from '@material-ui/core/Switch';
 
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { useDispatch, useSelector } from 'react-redux';
 
-import connectComponent from '../../../helpers/connect-component';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 import {
   enqueueRequestRestartSnackbar,
@@ -26,61 +25,45 @@ import hunspellLanguagesMap from '../../../constants/hunspell-languages';
 
 import DialogSpellcheckLanguages from './dialog-spellcheck-languages';
 
-const SectionLanguage = ({
-  spellcheck,
-  spellcheckLanguages,
-  onOpenDialogSpellcheckLanguages,
-}) => (
-  <>
-    <List disablePadding dense>
-      <ListItem>
-        <ListItemText primary="Spell check" />
-        <ListItemSecondaryAction>
-          <Switch
-            edge="end"
-            color="primary"
-            checked={spellcheck}
-            onChange={(e) => {
-              requestSetPreference('spellcheck', e.target.checked);
-              enqueueRequestRestartSnackbar();
-            }}
-          />
-        </ListItemSecondaryAction>
-      </ListItem>
-      {window.process.platform !== 'darwin' && (
-        <>
-          <Divider />
-          <ListItem button onClick={onOpenDialogSpellcheckLanguages}>
-            <ListItemText
-              primary="Spell checking languages"
-              secondary={spellcheckLanguages.map((code) => hunspellLanguagesMap[code]).join(' | ')}
+const SectionLanguage = () => {
+  const dispatch = useDispatch();
+
+  const spellcheck = useSelector((state) => state.preferences.spellcheck);
+  const spellcheckLanguages = useSelector((state) => state.preferences.spellcheckLanguages);
+
+  return (
+    <>
+      <List disablePadding dense>
+        <ListItem>
+          <ListItemText primary="Spell check" />
+          <ListItemSecondaryAction>
+            <Switch
+              edge="end"
+              color="primary"
+              checked={spellcheck}
+              onChange={(e) => {
+                requestSetPreference('spellcheck', e.target.checked);
+                enqueueRequestRestartSnackbar();
+              }}
             />
-            <ChevronRightIcon color="action" />
-          </ListItem>
-        </>
-      )}
-    </List>
-    <DialogSpellcheckLanguages />
-  </>
-);
-
-SectionLanguage.propTypes = {
-  onOpenDialogSpellcheckLanguages: PropTypes.func.isRequired,
-  spellcheck: PropTypes.bool.isRequired,
-  spellcheckLanguages: PropTypes.arrayOf(PropTypes.string).isRequired,
+          </ListItemSecondaryAction>
+        </ListItem>
+        {window.process.platform !== 'darwin' && (
+          <>
+            <Divider />
+            <ListItem button onClick={dispatch(openDialogSpellcheckLanguages)}>
+              <ListItemText
+                primary="Spell checking languages"
+                secondary={spellcheckLanguages.map((code) => hunspellLanguagesMap[code]).join(' | ')}
+              />
+              <ChevronRightIcon color="action" />
+            </ListItem>
+          </>
+        )}
+      </List>
+      <DialogSpellcheckLanguages />
+    </>
+  );
 };
 
-const mapStateToProps = (state) => ({
-  spellcheck: state.preferences.spellcheck,
-  spellcheckLanguages: state.preferences.spellcheckLanguages,
-});
-
-const actionCreators = {
-  openDialogSpellcheckLanguages,
-};
-
-export default connectComponent(
-  SectionLanguage,
-  mapStateToProps,
-  actionCreators,
-);
+export default SectionLanguage;
