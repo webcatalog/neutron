1ce809925de290e45d9fcb9a74278bd48247b6cc
dotrongkhang2000 <76832829+dotrongkhang2000@users.noreply.github.com>
2022-03-23 18:43:49 +0700

    First file change to sx prop

--- src/components/preferences/section-search/index.js
+++ src/components/preferences/section-search/index.js
@@ -8,7 +8,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
-import makeStyles from '@mui/styles/makeStyles';
 
 import { useSelector } from 'react-redux';
 
@@ -18,22 +17,7 @@ import {
 
 import searchEngines from '../../../constants/search-engines';
 
-const useStyles = makeStyles((theme) => ({
-  selectRoot: {
-    borderRadius: theme.spacing(0.5),
-    fontSize: '0.84375rem',
-  },
-  select: {
-    paddingTop: theme.spacing(1),
-    paddingRight: 26,
-    paddingBottom: theme.spacing(1),
-    paddingLeft: theme.spacing(1.5),
-  },
-}));
-
 const SectionSearch = () => {
-  const classes = useStyles();
-
   const searchEngine = useSelector((state) => state.preferences.searchEngine);
 
   return (
@@ -49,10 +33,13 @@ const SectionSearch = () => {
           variant="filled"
           disableUnderline
           margin="dense"
-          classes={{
-            root: classes.select,
+          sx={{
+            borderRadius: 0.5,
+            fontSize: '0.84375rem',
+            '& .MuiSelect-select': {
+              py: 1, pl: 1.5, pr: 26,
+            },
           }}
-          className={classes.selectRoot}
         >
           {Object.keys(searchEngines).map((optKey) => {
             const opt = searchEngines[optKey];
