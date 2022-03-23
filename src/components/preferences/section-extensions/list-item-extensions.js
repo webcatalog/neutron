28c5417cdbe817fee420c7569f53b2f961b4e8d2
Quang Lam <quang.lam2807@gmail.com>
2022-03-22 21:47:34 -0700

    Update list-item-extensions.js

--- src/components/preferences/section-extensions/list-item-extensions.js
+++ src/components/preferences/section-extensions/list-item-extensions.js
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import React, { useState, useEffect, useMemo, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { uniqBy } from 'lodash';
 import { app } from '@electron/remote';
 
