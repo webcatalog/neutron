a4f9a63858f0c2da39d148144a7fc5b0599f7bb0
Quang Lam <quang.lam2807@gmail.com>
2022-04-01 11:51:36 -0500

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 declare global {
   type SearchEngineId =
     'google' | 'baidu' | 'yahoo' | 'bing' | 'yandex' | 'sogou' | 'duckduckgo'
